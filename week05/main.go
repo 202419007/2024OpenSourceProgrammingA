9d3c51fa3467d91c058c1f0fbc5116e6963900b8
202419007 <202419007@itc.ac.kr>
2024-09-27 12:24:19 +0900

    v0.3 Type

--- week05/main.go
+++ week05/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	var i int = 55
-	fmt.Printf("value i : %d\n", i)
+	//var i int = 55
+
+	// var i int
+	// i = 55
+
+	var f float32 = 4.30 // f := 4.30 // float64
+	i := 55
+
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Printf("%s\n", strings.Title("inhatc"))
+	fmt.Println(math.Ceil(3.99))
+
+	fmt.Printf("value i : %d\n", i) // 숫자만 가능
 	fmt.Print("value i : ", i, "\n")
 	fmt.Println("value i : ", i)
 }

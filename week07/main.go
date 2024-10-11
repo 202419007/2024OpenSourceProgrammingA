ce5ec0fd2c3d479d0b99502de1d3aa3635f14eee
202419007 <202419007@itc.ac.kr>
2024-10-11 11:17:50 +0900

    v1.0 Time

--- week07/main.go
+++ week07/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"time"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	fmt.Println("Input your name : ")
-	name, err := in.ReadString('\n')
-	fmt.Println(name)
-	fmt.Println(err)
+	var now time.Time = time.Now()
+	//var month int = int(now.Month())
+	fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", now.Year(), int(now.Month()), now.Day())
+	fmt.Printf("지금은 시각은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
 }

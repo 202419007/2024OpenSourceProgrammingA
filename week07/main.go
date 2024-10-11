558a6457c2f703a458b569ac331a37bb347fe37b
202419007 <202419007@itc.ac.kr>
2024-10-11 12:06:20 +0900

    v1.5 Scope

--- week07/main.go
+++ week07/main.go
@@ -17,13 +17,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	i = strings.TrimSpace(i)                  // python strip.
-	score, err := strconv.ParseInt(i, 16, 32) // 문자열 변수 i의 값을 정수형(32비트)으로 변환, 입력받은 값은 16진수로 처리
-	if score >= 60 {
-		fmt.Println("A")
-		fmt.Printf("%d\n", score)
+	i = strings.TrimSpace(i)                  // python strip
+	score, err := strconv.ParseInt(i, 10, 32) // 문자열 변수 i의 값을 정수형(32비트)으로 변환, 입력받은 값은 16진수로 처리
+	var aOrNot string
+	if score >= 90 {
+		aOrNot = "A"
 	} else {
-		fmt.Println("BCDF")
-		fmt.Printf("%d\n", score)
+		aOrNot = "BCDF"
 	}
+	fmt.Printf("%d점은 %s등급입니다\n", score, aOrNot)
 }

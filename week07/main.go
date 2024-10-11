b87794fc3b6457fde53b28257eceb29c26e9ad8a
202419007 <202419007@itc.ac.kr>
2024-10-11 12:01:04 +0900

    v1.4 Parse

--- week07/main.go
+++ week07/main.go
@@ -17,7 +17,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	i = strings.TrimSpace(i)                  // python strip
+	i = strings.TrimSpace(i)                  // python strip.
 	score, err := strconv.ParseInt(i, 16, 32) // 문자열 변수 i의 값을 정수형(32비트)으로 변환, 입력받은 값은 16진수로 처리
 	if score >= 60 {
 		fmt.Println("A")

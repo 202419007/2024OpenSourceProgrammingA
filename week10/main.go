68fc9a410f27293e25e3e10756441556c78fc678
202419007 <202419007@itc.ac.kr>
2024-11-01 11:35:54 +0900

    2.8 Prime Number

--- week10/main.go
+++ week10/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -23,16 +22,16 @@ func main() {
 
 	var isPrime bool = true
 
-	// 논리적 오류 해결
 	if n < 2 {
 		isPrime = false
 	} else {
-		for j := 2; j <= int(math.Sqrt(float64(n))); j++ {
+		//for j := 2; j <= int(math.Sqrt(float64(n))); j++ {
+		for j := 2; j*j <= n; j++ {
 			if n%j == 0 {
 				isPrime = false
-				break // 무의미한 반복을 제거
+				break
 			}
-			fmt.Printf("%d", j) // 반복문 횟수 확인용 코드
+			fmt.Printf("%d", j)
 		}
 	}
 

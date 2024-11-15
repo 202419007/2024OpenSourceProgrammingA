02e036fff6d86b778637d638c0f51696e248e163
202419007 <202419007@itc.ac.kr>
2024-11-15 10:52:14 +0900

    v3.7 Array

--- week12/main.go
+++ week12/main.go
@@ -6,26 +6,17 @@ import (
 )
 
 func main() {
-	// var dates [3]time.Time
-	// dates[1] = time.Unix(1447920000, 0)
-	// fmt.Println(dates[1])
-
-	// var dates [3]time.Time = [3]time.Time(time.Unix(0, 0), time.Unix(1, 0), time.Unix(1947920001, 0))
-	// fmt.Println(dates[0], dates[1], dates[2])
-
-	// dates := [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1947920001, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2])
-
-	// dates := [3]time.Time{
-	// 	time.Unix(0, 0),
-	// 	time.Unix(1, 0),
-	// 	time.Unix(1947920001, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2])
 
 	dates := [3]time.Time{
 		time.Unix(0, 0),
 		time.Unix(1, 0),
 		time.Unix(1947920001, 0), // 쉼표가 반드시 필요(need comma)
 	}
-	fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Println(dates[0], dates[1], dates[2])  // array elements
+	//fmt.Println(dates)  // array
+	//fmt.Printf("%#v\n", dates)  // array literal
+
+	for i := 0; i < len(dates); i++ {
+		fmt.Println(i, dates[i])
+	}
 }

ce1f95826fa63dc80ccd651dd669d6c06733eda2
202419007 <202419007@itc.ac.kr>
2024-11-29 11:53:03 +0900

    v5.0 Struct

--- week14/main.go
+++ week14/main.go
@@ -2,22 +2,41 @@ package main
 
 import "fmt"
 
-type student struct {
-	id   int
-	name string
-	gpa  float32
+type visitor struct {
+	age  int
+	cost int
+}
+
+func calculateCost(visitors []visitor) int {
+	totalCost := 0
+	for _, v := range visitors {
+		totalCost = totalCost + v.cost
+	}
+	return totalCost
 }
 
 func main() {
-	var student1 student
-	student1.id = 20241234
-	student1.name = "Kim"
-	student1.gpa = 4.5
-	fmt.Println(student1.gpa)
+	var numVisitors int
+	fmt.Println("How many visitors? ")
+	fmt.Scanln(&numVisitors)
 
-	var student2 student
-	student2.id = 20244321
-	student2.name = "King"
-	student2.gpa = 4.7
-	fmt.Println(student2.id)
+	var vs []visitor // 구조체 슬라이스 변수
+	vs = make([]visitor, numVisitors)
+
+	for i := 0; i < numVisitors; i++ {
+		var age int
+		fmt.Print("Input age : ")
+		fmt.Scanln(&age)
+
+		switch {
+		case age < 12:
+			vs[i] = visitor{age: age, cost: 5000}
+		case age >= 12 && age < 65:
+			vs[i] = visitor{age: age, cost: 10000}
+		default: // over 65 years old
+			vs[i] = visitor{age: age, cost: 7000}
+		}
+	}
+
+	fmt.Printf("total price is %dwon.", calculateCost(vs))
 }
